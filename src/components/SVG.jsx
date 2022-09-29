64fddcde30d8069bbd789281e42a41565caf0bd2
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2022-09-29 23:38:58 +0100

    added svg component

---
+++ src/components/SVG.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Svg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="935"
+      height="701"
+      viewBox="0 0 935 701"
+      id="avatar-shapes"
+      data-shape="true"
+    >
+      <path
+        fill="none"
+        stroke="#0b0b0b"
+        strokeMiterlimit="50"
+        strokeWidth="2"
+        d="M2.114 541.705v0l-.56 22.258v0l22.258-.561v0l-.56 22.259v0l22.258-.56v0l-.56 22.258v0l22.258-.56v0l-.559 22.26v0l22.26-.56v0"
+      ></path>
+    </svg>
+  );
+}
+
+export default Svg;
---
+++ src/components/SVG2.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Svg2() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="935"
+      height="701"
+      viewBox="0 0 935 701"
+      id="avatar-shapes"
+      data-shape="true"
+    >
+      <path
+        fill="none"
+        stroke="#4831d4"
+        strokeMiterlimit="50"
+        strokeWidth="2"
+        d="M846.646 332.048v0l22.258.562v0l-.56-22.26v0l22.258.56v0l-.56-22.258v0l22.259.56v0l-.56-22.257v0l22.26.559v0l-.56-22.26v0"
+      ></path>
+    </svg>
+  );
+}
+
+export default Svg2;
