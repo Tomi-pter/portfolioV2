2b82e6cad69dfaa22446c046092255a6f3809ef5
Tomi Aiyeniko <tomiaiyeniko@gmail.com>
2023-10-05 11:18:53 +0100

    changed linkedin link

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,39 +1,39 @@
-import { WorkStyled } from './styled/WorkStyle';
-import kanban from '../assets/kanban/kanban.webp';
+import { WorkStyled } from "./styled/WorkStyle";
+import kanban from "../assets/kanban/kanban.webp";
 // import rock from '../assets/rock/rock.webp';
 // import room from '../assets/room/room.webp';
-import rest from '../assets/rest/rest.webp';
-import designo from '../assets/designo/designo.webp';
-import pharma from '../assets/pharma/pharma_thumbnail.png';
-import serviceUp from '../assets/serviceup/serviceup_logo.svg';
-import { Github } from '@styled-icons/boxicons-logos/Github';
-import { Linkedin } from '@styled-icons/boxicons-logos/Linkedin';
-import { OpenInNew } from '@styled-icons/material-rounded/OpenInNew';
-import { useInView } from 'react-intersection-observer';
+import rest from "../assets/rest/rest.webp";
+import designo from "../assets/designo/designo.webp";
+import pharma from "../assets/pharma/pharma_thumbnail.png";
+import serviceUp from "../assets/serviceup/serviceup_logo.svg";
+import { Github } from "@styled-icons/boxicons-logos/Github";
+import { Linkedin } from "@styled-icons/boxicons-logos/Linkedin";
+import { OpenInNew } from "@styled-icons/material-rounded/OpenInNew";
+import { useInView } from "react-intersection-observer";
 // import Svg from "./SVG";
 // import Svg2 from "./SVG2";
 
 function Work() {
   let interval;
 
-  const originalName = 'Aiyeniko Tomi Peter.';
+  const originalName = "Aiyeniko Tomi Peter.";
 
   const randomInt = (max) => Math.floor(Math.random() * max);
   const randomFromArray = (array) => array[randomInt(array.length)];
 
   const scrambleText = (text) => {
-    const chars = '*?><[]&@#)(.%$-_:/;?!'.split('');
+    const chars = "*?><[]&@#)(.%$-_:/;?!".split("");
     return text
-      .split('')
+      .split("")
       .map((x) => (randomInt(3) > 1 ? randomFromArray(chars) : x))
-      .join('');
+      .join("");
   };
 
   const scramble = (e) => {
     let name = document.querySelector(`.${e.target.className}`);
     let nameClass = document.querySelector(`.${e.target.className}`).className;
 
-    nameClass === 'name' &&
+    nameClass === "name" &&
       (interval = setInterval(
         () => (name.textContent = scrambleText(originalName)),
         125
@@ -50,58 +50,58 @@ function Work() {
 
   const works = [
     {
-      name: 'ServiceUp',
-      desc: 'ServiceUp is the easiest way to get your car repaired. Sit back and relax while we pick up, repair, and drop your car back off to you.',
-      tools: ['Webflow', 'GSAP', 'JavaScript', 'Zapier', 'Tally'],
+      name: "ServiceUp",
+      desc: "ServiceUp is the easiest way to get your car repaired. Sit back and relax while we pick up, repair, and drop your car back off to you.",
+      tools: ["Webflow", "GSAP", "JavaScript", "Zapier", "Tally"],
       img: serviceUp,
       // github: 'https://github.com/Tomi-pter/room-homepage.git',
-      live: 'https://www.serviceup.com/'
+      live: "https://www.serviceup.com/"
     },
     {
-      name: 'PremierPharma WebSite & WebApp',
-      desc: 'The pharmaceutical wholesaler you trust.',
+      name: "PremierPharma WebSite & WebApp",
+      desc: "The pharmaceutical wholesaler you trust.",
       tools: [
-        'Webflow',
-        'React.js',
-        'Redux',
-        'SCSS',
-        'Styled Components',
-        'JQuery',
-        'React Router'
+        "Webflow",
+        "React.js",
+        "Redux",
+        "SCSS",
+        "Styled Components",
+        "JQuery",
+        "React Router"
       ],
       img: pharma,
       // github: 'https://github.com/Tomi-pter/room-homepage.git',
-      live: 'https://app.premierpharma.com/login'
+      live: "https://app.premierpharma.com/login"
     },
     {
-      name: 'Kanban Task Management App',
-      desc: 'Create boards with specific tasks, sort into groups and break into subtasks, drag and drop tasks. The perfect productivity tool.',
+      name: "Kanban Task Management App",
+      desc: "Create boards with specific tasks, sort into groups and break into subtasks, drag and drop tasks. The perfect productivity tool.",
       tools: [
-        'TypeScript',
-        'React.js',
-        'Redux',
-        'Styled Components',
-        'React-beautiful-dnd'
+        "TypeScript",
+        "React.js",
+        "Redux",
+        "Styled Components",
+        "React-beautiful-dnd"
       ],
       img: kanban,
-      github: 'https://github.com/Tomi-pter/kanban-ts.git',
-      live: 'https://tomi-task-manager.netlify.app/'
+      github: "https://github.com/Tomi-pter/kanban-ts.git",
+      live: "https://tomi-task-manager.netlify.app/"
     },
     {
-      name: 'Designo Agency website',
-      desc: '9 page Designo agency website, includes dedicated pages for services rendered, contact, company description and location.',
-      tools: ['Next.js', 'Styled Components', 'Framer-Motion'],
+      name: "Designo Agency website",
+      desc: "9 page Designo agency website, includes dedicated pages for services rendered, contact, company description and location.",
+      tools: ["Next.js", "Styled Components", "Framer-Motion"],
       img: designo,
-      github: 'https://github.com/Tomi-pter/designo-multipage-website.git',
-      live: 'https://tomi-designo.vercel.app/'
+      github: "https://github.com/Tomi-pter/designo-multipage-website.git",
+      live: "https://tomi-designo.vercel.app/"
     },
     {
-      name: 'REST Countries App',
-      desc: 'A site that consumes the RESTcountriesV3 api to display vital information on all countries. Useful tool for travellers.',
-      tools: ['React.js', 'TailwindCSS', 'React Router'],
+      name: "REST Countries App",
+      desc: "A site that consumes the RESTcountriesV3 api to display vital information on all countries. Useful tool for travellers.",
+      tools: ["React.js", "TailwindCSS", "React Router"],
       img: rest,
-      github: 'https://github.com/Tomi-pter/REST-countries-app.git',
-      live: 'https://tomi-know-your-countries.netlify.app/'
+      github: "https://github.com/Tomi-pter/REST-countries-app.git",
+      live: "https://tomi-know-your-countries.netlify.app/"
     }
     // {
     //   name: 'Rock, Paper, Scissors',
@@ -114,7 +114,7 @@ function Work() {
   ];
 
   const { ref: meRef, inView: meVisible } = useInView({
-    rootMargin: '0px 0px 20px 0px'
+    rootMargin: "0px 0px 20px 0px"
   });
   const { ref: work1Ref, inView: work1Visible } = useInView({ threshold: 0.5 });
   const { ref: work2Ref, inView: work2Visible } = useInView({ threshold: 0.3 });
@@ -131,33 +131,33 @@ function Work() {
 
   meVisible &&
     document
-      ?.querySelectorAll('.icon')
-      .forEach((ico) => ico.classList?.remove('side'));
+      ?.querySelectorAll(".icon")
+      .forEach((ico) => ico.classList?.remove("side"));
   !meVisible &&
     document
-      ?.querySelectorAll('.icon')
-      .forEach((ico) => ico.classList?.add('side'));
+      ?.querySelectorAll(".icon")
+      .forEach((ico) => ico.classList?.add("side"));
 
   work1Visible &&
     document
-      ?.querySelector('.specific:first-of-type')
-      ?.classList?.add('inView');
+      ?.querySelector(".specific:first-of-type")
+      ?.classList?.add("inView");
   work2Visible &&
     document
-      ?.querySelector('.specific:nth-of-type(2)')
-      ?.classList?.add('inView');
+      ?.querySelector(".specific:nth-of-type(2)")
+      ?.classList?.add("inView");
   work3Visible &&
     document
-      ?.querySelector('.specific:nth-of-type(3)')
-      ?.classList?.add('inView');
+      ?.querySelector(".specific:nth-of-type(3)")
+      ?.classList?.add("inView");
   work4Visible &&
     document
-      ?.querySelector('.specific:nth-of-type(4)')
-      ?.classList?.add('inView');
+      ?.querySelector(".specific:nth-of-type(4)")
+      ?.classList?.add("inView");
   work5Visible &&
-    document?.querySelector('.specific:last-of-type')?.classList?.add('inView');
+    document?.querySelector(".specific:last-of-type")?.classList?.add("inView");
   asideVisible &&
-    document?.querySelector('#work aside')?.classList?.add('inView');
+    document?.querySelector("#work aside")?.classList?.add("inView");
 
   // !icon1Visible &&
   //   document?.querySelector("#work .icon:first-of-type")?.classList?.add("out");
@@ -195,10 +195,7 @@ function Work() {
         <p className="engr" ref={meRef}>
           Software Engineer <span className="rocket">🚀</span>
         </p>
-        <a
-          href="https://www.linkedin.com/in/tomi-aiyeniko-725860209/"
-          className="icon"
-        >
+        <a href="https://www.linkedin.com/in/tomi-aiyeniko" className="icon">
           {/* <span className="slide"></span> */}
           <Linkedin />
         </a>
@@ -242,7 +239,7 @@ function Work() {
                       </a>
                       <a href={live}>
                         <OpenInNew />
-                      </a>{' '}
+                      </a>{" "}
                     </>
                   ) : (
                     <a href={live}>
